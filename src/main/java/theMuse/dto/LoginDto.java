f6974017e32a432f08d07bedcd13823feb5f6df9
Kieun Moon <102676794+kieunMoon@users.noreply.github.com>
2023-02-13 17:16:05 +0900

    02/13 17:15
    ㅋㅋ

---
+++ src/main/java/theMuse/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package theMuse.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+	private String userId;
+	private String userPassword;
+}
---
+++ src/main/java/theMuse/mapper/LoginMapper.java
@@ -0,0 +1,14 @@
+package theMuse.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import theMuse.dto.LoginDto;
+import theMuse.dto.UserDto;
+
+@Mapper
+public class LoginMapper {
+	public UserDto login(LoginDto loginDto) throws Exception;
+	public int registUser(UserDto userDto) throws Exception;
+	public UserDto selectUserByUserId(String userId);
+	
+}
